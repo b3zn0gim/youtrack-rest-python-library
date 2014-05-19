db64ea466aea44eda80e6bb819c787b55785e1f2
Krzysztof Krzysztofik <krzysztof.krzysztofik@teonite.com>
2014-05-19 17:56:58 +0200

    package youtrack connection lib

--- python/setup.py
+++ python/setup.py
@@ -1,1 +1,23 @@
 __author__ = 'kkrzysztofik'
+
+from setuptools import setup
+from version import version
+
+setup(
+    name='YouTrack-Python',
+    version=version,
+    author='JetBrains s.r.o',
+    packages=['youtrack'],
+    url='http://www.jetbrains.com/youtrack/',
+    description='Python library that wraps YouTrack REST API.',
+    license='Apache License, Version 2.0',
+    install_requires=[
+        "httplib2 >= 0.7.4",
+        "pyactiveresource >= 2.0.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7"
+    ]
+)
